a7726c3053612ec3013ca3a4ddbae30367abce87
harleyliao <357857613@qq.com>
2024-01-29 03:31:26 +0000

    add bin search

---
+++ src/704_bin_search.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+
+using std::vector;
+
+int search(vector<int>& nums, int target) {
+    int i = 0;
+    int j = nums.size() - 1;
+    while (i <= j) {
+        int mid = (i+j) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+        if (nums[mid] > target) {
+            j = mid - 1;
+        } else {
+            i = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    std::vector<int> nums = {-1,0,3,5,9,12};
+    int num = search(nums, 3);
+    std::cout << num << std::endl;
+}
